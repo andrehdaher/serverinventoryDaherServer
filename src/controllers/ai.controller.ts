3a4ab313864c2f9765ba8c25017f4569fd615116
Andreh Daher <andrehdaher2003@gmail.com>
2026-05-20 09:52:53 +0300

    ai

--- src/controllers/ai.controller.ts
+++ src/controllers/ai.controller.ts
@@ -597,7 +597,7 @@ const getLatestSnapshotReport = async (): Promise<LatestReportResult | null> => 
 
 export const saveAiReport = async (req: Request, res: Response) => {
   try {
-    const aiResponse = req.body;
+    const aiResponse = req.body.item;
     const saveAi = JSON.stringify(aiResponse[0].output[0].content[0].text, null, 2).toLowerCase()
     console.log("Received AI response for saving:", saveAi);
     console.log("Received AI response for asasasasa:", JSON.stringify(aiResponse, null, 2));
